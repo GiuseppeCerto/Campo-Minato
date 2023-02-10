745ea81eefa2522c44559632fd6edb0abfd4a8b3
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-10 20:39:11 +0100

    EDIT end game after click on bomb cell

--- js/app.js
+++ js/app.js
@@ -1,83 +1,96 @@
 let bottonStart = document.getElementById('gameStartBtn')
 
-bottonStart.addEventListener('click', gameMineflid {
+bottonStart.addEventListener('click', gameMinefield)
 
-    function gameMineflid () {
+function gameMinefield (event) {
 
-        bottonStart.innerHTML = 'Reset'
+    bottonStart.innerHTML = 'Reset'
 
-        let sideNumberOfCells = document.getElementById('levelRange')
+    let sideNumberOfCells = document.getElementById('levelRange')
 
-        let numberOfCells = sideNumberOfCells.value * sideNumberOfCells.value
+    let numberOfCells = sideNumberOfCells.value * sideNumberOfCells.value
 
-        const grillElement = document.getElementById('grill')
+    const grillElement = document.getElementById('grill')
 
-        grillElement.innerHTML = ''
+    grillElement.innerHTML = ''
+    
+    for (let i = 0; i < numberOfCells; i++) {
+
+        let numberCycle = i + 1
+
+        const cellElement = (`<div class="cell" style="width: calc(100% / ${sideNumberOfCells.value}); font-size: 15px;">${numberCycle}</div>`);
+
+        grillElement.innerHTML += cellElement
+    }
+
+    const cellElements = document.querySelectorAll('.cell')
+
+    for (let i = 0; i < cellElements.length; i++) {
+
+        const cell = cellElements[i]
+
+        cell.addEventListener('click', cellClicked)
+    }
+
+    function cellClicked (event){
+
+        let cell = event.target
+
+        cell.classList.add('bg-warning')
+
+        cell.classList.add('cell-style')
+
+        cell.removeEventListener('click', cellClicked)
+
+        let numberCell = parseInt(cell.innerHTML)
+
+        cell.innerHTML = `<i class="fa-solid fa-hands-clapping"></i>`
         
-        for (let i = 0; i < numberOfCells; i++) {
 
-            let numberCycle = i + 1
 
-            const cellElement = (`<div class="cell" style="width: calc(100% / ${sideNumberOfCells.value}); font-size: 12px;">${numberCycle}</div>`);
+        // seganpunti **************************************
 
-            grillElement.innerHTML += cellElement
-        }
 
-        const cellElements = document.querySelectorAll('.cell')
+        
+        let points = document.getElementById('point-section')
 
-        for (let i = 0; i < cellElements.length; i++) {
+        points = points =+ 1
 
-            const cell = cellElements[i]
+        points.innerHTML
+        
+        if(bombsBox.includes(numberCell)){
 
-            cell.addEventListener('click', cellClicked)
-        }
+            cell.classList.remove('bg-warning')
 
-        function cellClicked (event){
+            cell.classList.add('cell-bomb')
 
-            const cell = event.target
+            cell.innerHTML = `<i class="fa-solid fa-bomb"></i>`
 
-            cell.classList.add('bg-warning')
+            window.alert("You Lose! Don't worry! Try Again! ")
 
-            cell.classList.add('cell-style')
+            for (let i = 0; i < cellElements.length; i++) {
 
-            cell.removeEventListener('click', cellClicked)
+                const cell = cellElements[i]
 
-            console.log(cell.innerHTML)
-
-            let numberCell = parseInt(cell.innerHTML)
-
-            console.log(numberCell)
-
-            console.log('Numero cella',numberCell)
-
-            cell.innerHTML = `<i class="fa-solid fa-hands-clapping"></i>`
-
-            if(bombsBox.includes(numberCell)){
-                cell.classList.remove('bg-warning')
-                cell.classList.add('cell-bomb')
-                cell.innerHTML = `<i class="fa-solid fa-bomb"></i>`
-            }
-        }
-
-        console.log(cellElements.length)
-
-        console.log(cellElements.length / 5)
-
-        bombsBox = []
-
-        console.log(bombsBox)
-
-        while(bombsBox.length <= (cellElements.length / 5)){
-
-            bombs = Math.floor(Math.random() * ((cellElements.length) - 1 + 1) + 1);
-
-            if(!bombsBox.includes(bombs)){
-
-                bombsBox.push(bombs)
+                cell.removeEventListener('click', cellClicked)
             }
         }
     }
-})   
+
+    bombsBox = []
+
+    while(bombsBox.length <= (cellElements.length / 5)){
+
+        bombs = Math.floor(Math.random() * ((cellElements.length) - 1 + 1) + 1);
+
+        if(!bombsBox.includes(bombs)){
+
+            bombsBox.push(bombs)
+        }
+    }
+
+    return
+}
 
 
 
