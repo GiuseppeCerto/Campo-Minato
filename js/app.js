fdff63063f3cc23ca4ad2292675b09be8270bfbf
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-11 16:48:52 +0100

    EDIT and game in case of victory

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ function gameMinefield (event) {
         const cellElement = (`<div class="cell" style="width: calc(100% / ${sideNumberOfCells.value}); font-size: 20px; text-shadow: 0px 0px #3c2424 !important;">${numberCycle}</div>`);
 
         grillElement.innerHTML += cellElement
+    
     }
 
     const cellElements = document.querySelectorAll('.cell')
@@ -31,6 +32,14 @@ function gameMinefield (event) {
 
         cell.addEventListener('click', cellClicked)
     }
+
+    let pointsScore = document.getElementById('point-section')
+
+    let points = 0
+
+    const winStep = []
+
+    const step = 'STEP'
 
     function cellClicked (event){
 
@@ -45,20 +54,14 @@ function gameMinefield (event) {
         let numberCell = parseInt(cell.innerHTML)
 
         cell.innerHTML = `<i class="fa-solid fa-helmet-un"></i>`
-        
 
+        points = points += 10
+    
+        pointsScore.innerHTML = 'Score: ' + points;
 
-        // seganpunti **************************************
-
-
-        
-        let points = document.getElementById('point-section')
-
-        points = points =+ 1
-
-        points.innerHTML
-        
-        if(bombsBox.includes(numberCell)){
+        winStep.push(step)
+    
+        if(bombsBox.includes(numberCell)){ 
 
             cell.classList.remove('bg-warning')
 
@@ -66,30 +69,50 @@ function gameMinefield (event) {
 
             cell.innerHTML = `<i class="fa-solid fa-bomb"></i>`
 
-            window.alert("You Lose! Don't worry! Try Again! ")
-
             for (let i = 0; i < cellElements.length; i++) {
 
                 const cell = cellElements[i]
 
                 cell.removeEventListener('click', cellClicked)
             }
+
+            pointsScore.innerHTML = 'You Lose! Try again'
+
+        } else if(winStep.length === cellElements.length - bombsBox.length) {
+
+            for (let i = 0; i < cellElements.length; i++) {
+
+                const cell = cellElements[i]
+
+                cell.removeEventListener('click', cellClicked)
+
+            }
+
+            pointsScore.innerHTML = 'You Win! ' + 'Score: ' + points;
         }
+
+        console.log(winStep)
+
+        console.log(cellElements.length - bombsBox.length)
+
+        console.log(bombsBox.length)
+
+        console.log(cellElements.length)
     }
 
-    bombsBox = []
+    const bombsBox = []
 
-    while(bombsBox.length <= (cellElements.length / 5)){
+    console.log(bombsBox)
 
-        bombs = Math.floor(Math.random() * ((cellElements.length) - 1 + 1) + 1);
+    while(bombsBox.length <= (cellElements.length / 10)){
+
+        const bombs = Math.floor(Math.random() * ((cellElements.length) - 1 + 1) + 1);
 
         if(!bombsBox.includes(bombs)){
 
             bombsBox.push(bombs)
         }
     }
-
-    return
 }
 
 
